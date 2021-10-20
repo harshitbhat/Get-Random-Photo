59f3019c929d0975f3cfd4c4ef81f68ba362402d
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-10-20 22:23:06 +0530

    Add Service to get image from API

--- src/app/componentss/show-photos/show-photos.component.ts
+++ src/app/componentss/show-photos/show-photos.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotosService } from 'src/app/services/photos.service';
 
 @Component({
   selector: 'app-show-photos',
   templateUrl: './show-photos.component.html',
-  styleUrls: ['./show-photos.component.css']
+  styleUrls: ['./show-photos.component.css'],
 })
 export class ShowPhotosComponent implements OnInit {
+  photoUrl: string = '';
+  photoDescription: string = '';
+  photographerFirstName: string = '';
+  photographerLastName: string = '';
+  photographerInstaProfile: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private photosService: PhotosService) {
+    this.fetchPhoto();
   }
 
+  ngOnInit(): void {}
+
+  onClick() {
+    this.fetchPhoto();
+  }
+
+  fetchPhoto() {
+    this.photosService.getPhoto().subscribe((res) => {
+      this.photoUrl = res.urls.regular;
+      this.photoDescription = res.description;
+      this.photographerFirstName = res.user.first_name;
+      this.photographerLastName = res.user.last_name;
+      this.photographerInstaProfile = res.user.social.portfolio_url;
+    });
+  }
 }
--- src/app/services/photos.service.ts
+++ src/app/services/photos.service.ts
@@ -2,6 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ACCESS_KEY } from 'src/assets/accessKey';
 
+interface UnsplashResponse {
+  description: string;
+  urls: {
+    regular: string;
+  };
+  user: {
+    first_name: string;
+    last_name: string;
+    social: {
+      portfolio_url: string;
+    };
+  };
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -9,10 +23,13 @@ export class PhotosService {
   constructor(private http: HttpClient) {}
 
   getPhoto() {
-    return this.http.get('https://api.unsplash.com/photos/random', {
-      headers: {
-        Authorization: `Client-ID ${ACCESS_KEY}`,
-      },
-    });
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization: `Client-ID ${ACCESS_KEY}`,
+        },
+      }
+    );
   }
 }
